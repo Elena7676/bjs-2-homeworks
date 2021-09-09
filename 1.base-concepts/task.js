eb34d54cd616f62a9a41213f2768319b0bfc8f3f
Elena Tarasova <tarasova7676@mail.ru>
2021-09-09 18:14:37 +0300

    task 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,22 @@
+
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
+  "use strict";
+  let d = b**2 - 4 * a * c;
+  arr = [];
+  if (d < 0) {
+    arr = [];
+  } else if (d == 0) {
+    let x = -b / (2 * a);
+    arr.push(x);
+  } else {
+    x1 = (-b + Math.sqrt(d) )/(2*a);
+    x2 = (-b - Math.sqrt(d) )/(2*a);
+    arr.push(x1);
+    arr.push(x2);
+  }
+
   return arr; // array
 }
 
