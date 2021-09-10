0302c63b7c212499dbd0c3ed011f1562e5846542
Elena Tarasova <tarasova7676@mail.ru>
2021-09-10 13:40:29 +0300

    task 2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,45 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+  let min, max, sum, avg;
+  min = 101;
+  max = -101;
+  sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    avg = (sum / arr.length).toFixed(2);
+    if (arr[i] > max) {
+      max = arr[i]
+    }
+    if (arr[i] < min) {
+      min = arr[i]
+    }
+  }
 
-  return { min:min, max:max, avg:avg };
+  return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
+  let max = 0;
 
-  // Ваш кода
-  // for ...
-  
+
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let element = arrOfArr[i];
+    worker(element);
+    if (worker(element) > max) {
+      max = worker(element)
+    }
+  }
+
   return max
-}
-
-// Задание 3
-function worker2(arr) {
-  // Ваш код
 }
